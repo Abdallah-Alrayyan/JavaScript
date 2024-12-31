60ebc5b12391e6eb79bd30d6d5693c6ac912ffd2
Abdallah-Alrayyan <aboodrayan300@gmail.com>
2024-12-31 05:26:11 +0300

    complete Day 1

---
+++ Day 1/task 1.js
@@ -0,0 +1,350 @@
+
+
+
+
+
+
+
+
+
+/*
+
+let x = 250 ;
+create variable Calculate the value of zakat for x ,If you know the percentage of zakat = 2.5 %; 
+
+let calc = x * 2.5 /100;
+console.log(calc);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Array Tasks : 
+// 1
+// Correct the syntax error
+
+//  let x = [ 1 , 7 , 9 , 45 ]
+
+//  let y =  ["Str", "alex","moh",'the','fox', 'over', "lazy", 'dog' ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+2
+What the index of "Banana","Tomato"
+var fruits=["Tomato","Banana","Watermelon"]
+
+
+
+var fruits=["Tomato","Banana","Watermelon"]
+
+index = [0 , 1 , 2]
+
+"Tomato" = index[0]
+
+"Banana"= index[1]
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+3
+Create an array represents your:
+1- Favorite Food (2)
+2- Favorite Sport (3)
+3- Favorite Movie (2)
+
+
+
+let food = [Mansaf , Sushi]
+let sport = [Football ,Basketball,Tennis]
+let movie = [Inception , The_Shawshank_Redemption]
+                
+*/                         
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+4
+Create a Variable to return the first element in an array 
+Ex: firstOfArray([1,4,5]) => 1
+Ex: firstOfArray(["t","u","g","x"]) => "t"
+
+
+
+let num = [1,4,5]
+let char = ["t","u","g","x"]
+
+let x = num[0];
+let y = char[0];
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+5
+Create a Variable to return the lastOfArray element in an array 
+
+Ex: lastOfArray([1,4,5]) => 5
+Ex: lastOfArray(["t","u","g","x"]) => "x"
+
+
+let num = [1,4,5]
+let char = ["t","u","g","x"]
+
+let x = num[num.length-1];
+let y = char[char.length-1];
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+6
+Using console make this array to be like this one (push, unshift, shift, pop)
+
+var array = [0,5,7,9]
+=> [1,3,4,6,8,9,10]
+
+
+let arr = [0,5,7,9]
+arr.shift()
+arr.shift()
+arr.shift()
+arr.unshift(1,2,3,4,5,6,7,8)
+arr.push(10)
+console.log(arr);
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+7
+Using the console try to figure out what the thing thats (push, unshift, shift, pop) return to you
+
+var array2 = [5,9,-7,3.5]
+
+push : add in the end
+unshift: add in the first
+shift: delete in the first
+pop: delete in the end
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+8.
+Write a JavaScript program to sort the items of an array.
+Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+Sample Output : -4,-3,1,2,3,5,6,7,8
+*/
+
+
+// let arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+
+// for (let i = 0; i < arr1.length; i++) {
+
+//     const element = arr1[i];
+
+//     for (let j = 0; j < arr1.length; j++) {
+//            if (element >= arr1[j]) {
+       
+//             }else {
+//               console.log([arr1[j] ,arr1[i] ]=  [arr1[i] ,arr1[j] ])  
+//               continue;
+//             }
+        
+//     }
+
+    
+// }
+
+// console.log(arr1.join(", "));
+
+
+
+
+
+
+
+// 9.
+// Write a JavaScript function that returns rate as text
+// less than 50 return ==> Fail
+// equal or between 50 and 59 ===> E
+// equal or between 60 and 69 ===> D
+// equal or between 70 and 79 ===> C
+// equal or between 80 and 89 ===> B
+// equal or between 90 and 100 ===> A
+
+// let gread = 600;
+// if(gread >= 90  && gread <= 100){
+
+//     document.write("A")
+
+// }else if(gread >= 80 && gread < 90){
+
+//     document.write("B")
+
+// }else if(gread >= 70 && gread < 80){
+
+//     document.write("C")
+
+    
+// }else if(gread >= 60 && gread < 70){
+    
+//     document.write("D")
+
+// }else if(gread >= 50 && gread < 60){
+
+//     document.write("E")
+
+    
+// }else if(gread < 50) {
+
+//     document.write("Fail")
+
+// }
+// else if(gread > 100) {
+
+//     document.write("must be less than 100")
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// 10.
+// Write a JS code to print a pattern using for loop
+
+//    1 
+//    1 2 
+//    1 2 3 
+//    1 2 3 4 
+//    1 2 3 4 5 
+//    1 2 3 4 5 6 
+//    1 2 3 4 5 6 7 
+//    1 2 3 4 5 6 7 8 
+
+// 
+// let x =0;
+// for(let i = 1; i < 9; i++){
+
+//     x = x + i +" " 
+
+//     console.log(x);
+   
+       
+// }
+ 
---
+++ Day 1/task 2.js
@@ -0,0 +1,16 @@
+
+// function fac(num){
+
+//     let result = 1;
+//     for (let i = 1; i <= num; i++) {
+
+//      result *= i; 
+      
+        
+//     }
+//     console.log(result) 
+// }
+
+// fac(5)
+
+
