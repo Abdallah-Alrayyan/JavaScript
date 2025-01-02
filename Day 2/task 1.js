f634a9f8be5ebdf50e8ed386a12be2147d68cc4c
Abdallah-Alrayyan <aboodrayan300@gmail.com>
2025-01-02 09:55:11 +0300

    complete Day 2

--- Day 2/task 1.js
+++ Day 2/task 1.js
@@ -18,14 +18,95 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 // let test =  document.getElementById("test")
-// let fonts = document.getElementById("fonts").value;
-// let fontSize = document.getElementById("fontSize").value;
+// let fonts = document.getElementById("fonts");
+// let fontSize = document.getElementById("fontSize");
 
 
-// let Italic = document.getElementById("Italic").checked;
-// let Bold = document.getElementById("Bold").checked;
-// let Underline = document.getElementById("Underline").checked;
+
+
+
+
+// fonts.value ='Times New Roman'; 
+// fonts.onfocus = function() { fonts.value =''; }; 
+// fonts.onblur = function() { fonts.value ='Times New Roman'; }; 
+
+
+// fontSize.value = '10px'
+// fontSize.onfocus = function () {fontSize.value  = '';}
+// fontSize.onblur = function () {fontSize.value  = '10px';}
+
+
+
+
+// console.log(fonts.value);
+// console.log(fontSize.value);
+
+
+// fonts.addEventListener('change' , () => {
+//     if (fonts.value == "fantasy") {
+//         test.style.fontFamily = "fantasy"
+//     }else {
+//         test.style.fontFamily = "'Times New Roman', Times, serif"
+//     }
+// })
+
+// fontSize.addEventListener('change' , () => {
+//     if (fontSize.value == "15px") {
+//         test.style.fontSize = "15px"
+//     }else {
+//         test.style.fontSize = "10px"
+//     }
+// })
+
+
+
+// let Italic = document.querySelector("input[name=Italic]")
+// let Bold = document.querySelector("input[name=Bold]")
+// let Underline = document.querySelector("input[name=Underline]")
+
+
+// Italic.addEventListener('change' , () => {
+//     if (Italic.checked) {
+//         test.style.fontStyle = "italic"
+//     }else {
+//         test.style.fontStyle = "normal"
+
+//     }
+// })
+
+// Bold.addEventListener('change' , () => {
+//     if (Bold.checked) {
+//         test.style.fontWeight = "bold"
+//     }else {
+//         test.style.fontWeight = "normal"
+
+//     }
+// })
+
+// Underline.addEventListener('change' , () => {
+//     if (Underline.checked) {
+//         test.style.textDecoration = "underline"
+//     }else {
+//         test.style.textDecoration = "none"
+
+//     }
+// })
+
+
+
+
 
 
 
