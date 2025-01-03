16589c2ca363eb24c01cef029151db20205756c5
Abdallah-Alrayyan <aboodrayan300@gmail.com>
2025-01-04 00:07:09 +0300

    complete Day 3

--- Day 3/task .js
+++ Day 3/task .js
@@ -726,7 +726,7 @@
 
 //     if (localStorage.getItem('howManyLinks') < 1) {
         
-//         localStorage.setItem('howManyLinks', Number(localStorage.getItem('howManyLinks')) + 1)
+//         localStorage.setItem('howManyLinks',  1)
 //     }
 //     else{
 //         localStorage.setItem('howManyLinks', Number(localStorage.getItem('howManyLinks')) + 1);
@@ -735,47 +735,47 @@
 // }
       
 
-// // counter()
-// // counter()
-// // counter()
-// // counter()
-// // counter()
+// counter()
+// counter()
+// counter()
+// counter()
+// counter()
 
 
 
 
 
-// /*
-// 24
-// Write a function called resetCounter
-// that will reset the previuos function
-// and return the number before reset and
-// a string say that the counter reset
+/*
+24
+Write a function called resetCounter
+that will reset the previuos function
+and return the number before reset and
+a string say that the counter reset
 
-// Ex: counter()
-// => 1
+Ex: counter()
+=> 1
 
-// Ex: counter()
-// => 2
+Ex: counter()
+=> 2
 
-// Ex: counter()
-// => 3
+Ex: counter()
+=> 3
 
-// Ex: resetCounter()
-// => 3 and the counter reset now
+Ex: resetCounter()
+=> 3 and the counter reset now
 
-// Ex: counter()
-// => 1
+Ex: counter()
+=> 1
 
-// Ex: counter()
-// => 2
+Ex: counter()
+=> 2
 
-// Ex: resetCounter()
-// => 2 and the counter reset now
+Ex: resetCounter()
+=> 2 and the counter reset now
 
-// Ex: counter()
-// => 1
-// */
+Ex: counter()
+=> 1
+*/
 
 
 
@@ -791,5 +791,8 @@
 // counter()
 // resetCounter()
 // counter()
+// counter()
+// counter()
+
 
 
